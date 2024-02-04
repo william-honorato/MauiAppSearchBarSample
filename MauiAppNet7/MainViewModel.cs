5d3eab89dfa4217f645550eac36d5726322ed7e9
william-honorato <37045327+william-honorato@users.noreply.github.com>
2024-02-04 13:17:55 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppNet7/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace MauiAppNet7
+{
+    internal class MainViewModel
+    {
+        public ICommand SearchOpenPage { get; private set; }
+        public MainViewModel()
+        {
+            SearchOpenPage = new Command(CommandSearch);
+        }
+
+        public async void CommandSearch()
+        {
+            var page = new ContentPage
+            {
+                Title = "Search Page Test",
+                Content = new Label
+                {
+                    Text = "New ContentPage"
+                }
+            };
+
+            await App.Current.MainPage.Navigation.PushAsync(page);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void Notify([CallerMemberName] string memberName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(memberName));
+        }
+
+        string _searchText = "";
+        public string SearchText
+        {
+            get { return _searchText; }
+            set
+            {
+                if (_searchText != value)
+                {
+                    _searchText = value;
+                    Notify();
+                }
+            }
+        }
+    }
+}
---
+++ MauiAppNet8/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppNet8
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainViewModel();
+        }
+    }
+}
